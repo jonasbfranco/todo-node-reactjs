91098a4bbb96cfb8aea99c2f3542aae049e644c4
jonasbfranco <jonasbfranco@hotmail.com>
2024-06-29 00:11:33 -0300

    finalizando frontend

--- todo-frontend/src/App.js
+++ todo-frontend/src/App.js
@@ -9,36 +9,52 @@ import axios from 'axios';
   { name: "Limpar o cachorro", status: false},
 ]; */
 
-const Todos = ({ todos }) => {
-  return (
-    <div className="todos"> 
-        {todos.map((todo) => {
-        return (
-          <div className="todo" key={todo.name}>
-            <button 
-              className='checkbox'
-              style={{backgroundColor: todo.status ? '#a879e6' : 'white'}}
-            ></button>
-            <p>{todo.name}</p>
-            <button>
-              <AiOutlineEdit 
-                color={"#64697b"}
-                size={20}
-              />
-            </button>
-            <button>
-              <AiOutlineDelete 
-                color={"#64697b"}
-                size={20}
-              />
-            </button>
-          </div>
-        )})}
-    </div>
-  );
-};
 
 function App() {
+
+  const Todos = ({ todos }) => {
+    return (
+      <div className="todos"> 
+          {todos.map((todo) => {
+          return (
+            <div className="todo" key={todo.name}>
+              <button
+                onClick={() => modifyStatusTodo(todo)} 
+                className='checkbox'
+                style={{backgroundColor: todo.status ? '#a879e6' : 'white'}}
+              ></button>
+              <p>{todo.name}</p>
+              <button
+                onClick={() => handleWithEditButtonClick(todo)}
+              >
+                <AiOutlineEdit 
+                  color={"#64697b"}
+                  size={20}
+                />
+              </button>
+              <button onClick={() => {deleteTodo(todo)}}>
+                <AiOutlineDelete 
+                  color={"#64697b"}
+                  size={20}
+                />
+              </button>
+            </div>
+          )})}
+      </div>
+    );
+  };
+
+  
+  async function handleWithNewButton() {
+    setInputVisibility(!inputVisibility);
+  };
+
+  async function handleWithEditButtonClick(todo) {
+    setSelectedTodo(todo);
+    setInputValue(todo.name);
+    //setInputVisibility(true);
+    setInputVisibility(!inputVisibility);
+  };
 
   async function getTodos() {
     const response = await axios.get('http://localhost:3333/todos');
@@ -46,7 +62,47 @@ function App() {
     //console.log(response.data);
   };
 
+  async function editTodo() {
+    await axios.put('http://localhost:3333/todos', {
+      id: selectedTodo.id,
+      name: inputValue,
+    });
+    setSelectedTodo();
+    setInputVisibility(false);
+    getTodos();
+    setInputValue("");
+  }
+
+  async function createTodo() {
+    const response = await axios.post('http://localhost:3333/todos', {
+      name: inputValue,
+    });
+    //getTodos();
+    //setinputVisibility(!inputVisibility);
+    setTodos([...todos, response.data]);
+    setInputValue("");
+    setInputVisibility(false);
+  };
+
+  async function deleteTodo(todo) {
+    await axios.delete(`http://localhost:3333/todos/${todo.id}`);
+    //setTodos(todos.filter((t) => t.id!== todo.id));
+    getTodos();
+  };
+
+  async function modifyStatusTodo(todo) {
+    await axios.put("http://localhost:3333/todos/", {
+      id: todo.id,
+      status:!todo.status,
+    });
+    getTodos();
+    //setTodos(todos.map((t) => t.id === todo.id? {...t, status:!t.status} : t));
+  };
+
   const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const [inputVisibility, setInputVisibility] = useState(false);
+  const [selectedTodo, setSelectedTodo] = useState();
 
   useEffect(() => {
     getTodos();
@@ -60,8 +116,16 @@ function App() {
         </div>
         {/* <Todos todos={arrayTodos}/> */}
         <Todos todos={todos}/>
-        <input className='inputName'></input>
-        <button className='newTaskButton'>+ New task</button>
+        <input 
+          value={inputValue} 
+          style={{display: inputVisibility ? "block" : "none"}}
+          onChange={(event) => {
+             setInputValue(event.target.value);
+          }}
+          className='inputName'>
+        </input>
+        <button onClick={inputVisibility ? selectedTodo ? editTodo : createTodo : handleWithNewButton} className='newTaskButton'>
+          {inputVisibility ? "Confirm" : "+ New task"}</button>
       </header>
     </div>
   );
